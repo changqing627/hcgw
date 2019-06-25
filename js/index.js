14d63cc0e427e9e21c9ec7c4f0d81412aa0fed76
changqing627 <2621008548@qq.com>
2019-06-25 17:49:36 +0800

    secend commit

---
+++ js/index.js
@@ -0,0 +1,40 @@
+$(function (){
+    //right 悬浮
+    for(var i=0; i<$(".infoList").length; i++){
+        var current=$(".infoList")[i];
+        console.log(current)
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+})
