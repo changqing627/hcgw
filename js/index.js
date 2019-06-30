f56844d7a72a6c50072ae30ab55647de7536516a
changqing627 <2621008548@qq.com>
2019-06-30 18:08:06 +0800

    fourth commit

--- js/index.js
+++ js/index.js
@@ -1,7 +1,16 @@
 $(function (){
     //right 悬浮
+    var $scrW=$(window).width();
+    // alert($scrW)
+    if($scrW>1080){
+        var $scrR=($scrW-1080)/2;
+        $(".xfbox").css('right',$scrR+'px')  
+    }
+   
+
    $(".infobox").find('li').each(function (){
        $(this).find('a').mouseover(function (){
+           alert(1)
            $(this).prev('p').css({'opacity':1});
        }).mouseout(function (){
            $(this).prev('p').css({'opacity':0})
