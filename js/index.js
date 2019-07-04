85026b34a2c0750ad157d9ff12e01d6147afb8fa
changqing627 <2621008548@qq.com>
2019-07-04 17:48:20 +0800

    sixth commit

--- js/index.js
+++ js/index.js
@@ -8,21 +8,6 @@ $(function (){
     }
    
 
-   $(".infobox").find('li').each(function (){
-
-    $(this).find('a').click(function (){
-        alert(1)
-    })
-
-    //    $(this).find('a').mouseover(function (){
-    //        alert(1)
-    //        $(this).prev('p').css({'opacity':1});
-    //    }).mouseout(function (){
-    //        $(this).prev('p').css({'opacity':0})
-    //    })
-
-   })
-
 
 
 
