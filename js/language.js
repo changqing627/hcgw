6dec8974de2ef80ea5cbfac4e274d9320905f972
changqing627 <2621008548@qq.com>
2019-07-10 18:06:31 +0800

    i18n02

--- js/language.js
+++ js/language.js
@@ -1,11 +1,10 @@
 jQuery(document).ready(function() {
     loadBundles('ch');
-
+    
     // configure language combo box
     jQuery('#lang').change(function() {
         var selection = jQuery('#lang option:selected').val();
         loadBundles(selection != 'browser' ? selection : null);
-
     });
 
 });
@@ -13,7 +12,7 @@ jQuery(document).ready(function() {
 function loadBundles(lang) {
     jQuery.i18n.properties({
         name:'Messages',
-        path:'../bundle/',
+        path:'/bundle/',
         mode:'both',
         language:lang,
         callback: function() {
@@ -23,9 +22,11 @@ function loadBundles(lang) {
 }
 
 function updateExamples() {
+   
 // 首页
     jQuery('#index01').empty().append(jQuery.i18n.prop('index01'));
     jQuery('#index02').empty().append(jQuery.i18n.prop('index02'));
+    jQuery('#index021').empty().append(jQuery.i18n.prop('index021'));
     jQuery('#index03').empty().append(jQuery.i18n.prop('index03'));
     jQuery('#index04').empty().append(jQuery.i18n.prop('index04'));
     jQuery('#index05').empty().append(jQuery.i18n.prop('index05'));
@@ -38,5 +39,90 @@ function updateExamples() {
     // 工业互联网+定制解决方案
     jQuery('#indus01').empty().append(jQuery.i18n.prop('indus01'));
     jQuery('#indus02').empty().append(jQuery.i18n.prop('indus02'));
+    jQuery('#indus03').empty().append(jQuery.i18n.prop('indus03'));
+    jQuery('#indus04').empty().append(jQuery.i18n.prop('indus04'));
+    jQuery('#indus041').empty().append(jQuery.i18n.prop('indus041'));
+    jQuery('#indus05').empty().append(jQuery.i18n.prop('indus05'));
+    jQuery('#indus06').empty().append(jQuery.i18n.prop('indus06'));
+    jQuery('#indus07').empty().append(jQuery.i18n.prop('indus07'));
+    jQuery('#indus08').empty().append(jQuery.i18n.prop('indus08'));
+    jQuery('#indus09').empty().append(jQuery.i18n.prop('indus09'));
+    jQuery('#indus10').empty().append(jQuery.i18n.prop('indus10'));
+    jQuery('#indus11').empty().append(jQuery.i18n.prop('indus11'));
+    jQuery('#indus12').empty().append(jQuery.i18n.prop('indus12'));
+    jQuery('#indus13').empty().append(jQuery.i18n.prop('indus13'));
+    jQuery('#indus14').empty().append(jQuery.i18n.prop('indus14'));
+    jQuery('#indus15').empty().append(jQuery.i18n.prop('indus15'));
+    jQuery('#indus16').empty().append(jQuery.i18n.prop('indus16'));
+    jQuery('#indus17').empty().append(jQuery.i18n.prop('indus17'));
+    jQuery('#indus18').empty().append(jQuery.i18n.prop('indus18'));
+    jQuery('#indus19').empty().append(jQuery.i18n.prop('indus19'));
+    jQuery('#indus20').empty().append(jQuery.i18n.prop('indus20'));
+    jQuery('#indus21').empty().append(jQuery.i18n.prop('indus21'));
+    jQuery('#indus22').empty().append(jQuery.i18n.prop('indus22'));
+    jQuery('#indus221').empty().append(jQuery.i18n.prop('indus221'));
+    jQuery('#indus23').empty().append(jQuery.i18n.prop('indus23'));
+    jQuery('#indus24').empty().append(jQuery.i18n.prop('indus24'));
+    jQuery('#indus241').empty().append(jQuery.i18n.prop('indus241'));
+    jQuery('#indus25').empty().append(jQuery.i18n.prop('indus25'));
+    jQuery('#indus26').empty().append(jQuery.i18n.prop('indus26'));
+    jQuery('#indus27').empty().append(jQuery.i18n.prop('indus27'));
+    jQuery('#indus28').empty().append(jQuery.i18n.prop('indus28'));
+    jQuery('#indus29').empty().append(jQuery.i18n.prop('indus29'));
+    jQuery('#indus30').empty().append(jQuery.i18n.prop('indus30'));
+    jQuery('#indus31').empty().append(jQuery.i18n.prop('indus31'));
+    jQuery('#indus32').empty().append(jQuery.i18n.prop('indus32'));
+    jQuery('#indus33').empty().append(jQuery.i18n.prop('indus33'));
+    jQuery('#indus34').empty().append(jQuery.i18n.prop('indus34'));
+    jQuery('#indus35').empty().append(jQuery.i18n.prop('indus35'));
+    jQuery('#indus36').empty().append(jQuery.i18n.prop('indus36'));
+    jQuery('#indus37').empty().append(jQuery.i18n.prop('indus37'));
+    jQuery('#indus38').empty().append(jQuery.i18n.prop('indus38'));
+    jQuery('#indus39').empty().append(jQuery.i18n.prop('indus39'));
+    jQuery('#indus40').empty().append(jQuery.i18n.prop('indus40'));
+    jQuery('#indus41').empty().append(jQuery.i18n.prop('indus41'));
+    jQuery('#indus42').empty().append(jQuery.i18n.prop('indus42'));
+    jQuery('#indus43').empty().append(jQuery.i18n.prop('indus43'));
+    jQuery('#indus44').empty().append(jQuery.i18n.prop('indus44'));
+    jQuery('#indus45').empty().append(jQuery.i18n.prop('indus45'));
+    jQuery('#indus46').empty().append(jQuery.i18n.prop('indus46'));
+    jQuery('#indus47').empty().append(jQuery.i18n.prop('indus47'));
+    jQuery('#indus48').empty().append(jQuery.i18n.prop('indus48'));
+    jQuery('#indus49').empty().append(jQuery.i18n.prop('indus49'));
+    jQuery('#indus50').empty().append(jQuery.i18n.prop('indus50'));
+    jQuery('#indus51').empty().append(jQuery.i18n.prop('indus51'));
+    jQuery('#indus52').empty().append(jQuery.i18n.prop('indus52'));
+    jQuery('#indus53').empty().append(jQuery.i18n.prop('indus53'));
+    jQuery('#indus54').empty().append(jQuery.i18n.prop('indus54'));
+    jQuery('#indus55').empty().append(jQuery.i18n.prop('indus55'));
+    jQuery('#indus56').empty().append(jQuery.i18n.prop('indus56'));
+    jQuery('#indus57').empty().append(jQuery.i18n.prop('indus57'));
+    // 关于我们
+    jQuery('#about01').empty().append(jQuery.i18n.prop('about01'));
+    jQuery('#about02').empty().append(jQuery.i18n.prop('about02'));
+    jQuery('#about03').empty().append(jQuery.i18n.prop('about03'));
+    jQuery('#about04').empty().append(jQuery.i18n.prop('about04'));
+    jQuery('#about05').empty().append(jQuery.i18n.prop('about05'));
+    jQuery('#about06').empty().append(jQuery.i18n.prop('about06'));
+    jQuery('#about07').empty().append(jQuery.i18n.prop('about07'));
+    jQuery('#about08').empty().append(jQuery.i18n.prop('about08'));
+    jQuery('#about09').empty().append(jQuery.i18n.prop('about09'));
+    jQuery('#about10').empty().append(jQuery.i18n.prop('about10'));
+    jQuery('#about11').empty().append(jQuery.i18n.prop('about11'));
+    jQuery('#about12').empty().append(jQuery.i18n.prop('about12'));
+    // footer
+    // jQuery('#foot01').empty().append(jQuery.i18n.prop('foot01'));
+    // jQuery('#foot02').empty().append(jQuery.i18n.prop('foot02'));
+    // jQuery('#foot03').empty().append(jQuery.i18n.prop('foot03'));
+    // jQuery('#foot04').empty().append(jQuery.i18n.prop('foot04'));
+    // jQuery('#foot05').empty().append(jQuery.i18n.prop('foot05'));
+    // jQuery('#foot06').empty().append(jQuery.i18n.prop('foot06'));
+    // jQuery('#foot07').empty().append(jQuery.i18n.prop('foot07'));
+    // jQuery('#foot08').empty().append(jQuery.i18n.prop('foot08'));
+    // jQuery('#foot09').empty().append(jQuery.i18n.prop('foot09'));
+    // jQuery('#foot10').empty().append(jQuery.i18n.prop('foot10'));
+    // jQuery('#foot11').empty().append(jQuery.i18n.prop('foot11'));
+    // jQuery('#foot12').empty().append(jQuery.i18n.prop('foot12'));
+
 
 }
